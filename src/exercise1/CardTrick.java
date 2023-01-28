c227cb66c1c285315b4ca01df8a84ed8726983d6
DarianTan <tandari@sheridancollege.ca>
2023-01-27 23:49:57 -0500

    I Am Done!

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -8,10 +8,7 @@ import java.util.Scanner;
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
- * @author Paul Bonenfant Jan 25, 2022
- * @author Aleksandar Manov Jan 27, 2023 
+ * @author Darian Tan January 27, 2023
  */
 public class CardTrick {
 
