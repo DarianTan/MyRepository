e15c7516dff21d63f33a4a5438877cd98b63e28d
DarianTan <tandari@sheridancollege.ca>
2023-01-27 23:52:05 -0500

    I Am Done.

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -147,7 +147,7 @@ public class CardTrick {
     /**
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
+     * @author Darian January 27, 2023
      */
     private static void printInfo() {
 
